70609776cba55f399eefeff4b2a15a62478237e4
Tuturu <ujeshman@gmail.com>
2018-08-08 18:33:49 -0400

    Proper file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const places = require('./data.json'),
       Command = require('command');
 
-module.exports = function JujuSpawn(dispatch) {
+module.exports = function SpawnChange(dispatch) {
 	const teleport_enabled = true, 			// Change this to false if you do not want this module on. (Might as well remove it at that point)
 		  auto_turnoff_time = 5;         // Time to Auto-Turn off in minutes.
 
@@ -14,55 +14,82 @@ module.exports = function JujuSpawn(dispatch) {
 
 	const command = Command(dispatch);
 	const turnoff = () => {
-		command.message('[JujuSpawn] Turning off following dungeon for safety reasons:');
-		for(place of places){
-			if(place.name != 'Ghiliglade' && place.active){
-				place.active = false;
-				command.message(' ' + place.name);
+		command.message('[Spawn Change] Turning off following dungeon bosses for safety reasons:');
+		for(const place of places){
+			for(const boss of Object.values(place.portTo)){
+				if(place.name != 'Ghiliglade' && boss.active){
+					boss.active = false;
+					command.message(' ' + boss.comment + ' of ' + place.name);
+				}
 			}
 		}
-		command.message('[JujuSpawn] Auto-turn off complete.');
+		command.message('[Spawn Change] Auto-turn off complete.');
 	};
-
-	command.add('SC', arg => {
-    	arg = arg.toUpperCase();
-    	if(arg === 'HELP'){
-    		let str = '[JujuSpawn] Possible dungeons are:';
+	const processCommand = (dungeon, whichBoss) => {
+		dungeon = dungeon.toUpperCase();
+    	if(dungeon === 'HELP'){
+    		let str = '[Spawn Change] Possible dungeons are:';
     		for(place of places) str = str + ' ' + place.short_name;
 			command.message(str);
 			return;
     	}
-    	for(place of places){
-			if(place.short_name === arg){
-				place.active = !place.active;
-				const status = place.active ? 'ON!' : 'OFF!'
-				command.message('[JujuSpawn] ' + place.name + ' Spawn has been turned ' + status + ' It will be turned off in ' + auto_turnoff_time + ' minutes for safety reasons!');
-				clearTimeout(timer);
-				timer = setTimeout(turnoff, auto_turnoff_time*60000);
-				return;
+    	if(whichBoss) whichBoss = whichBoss.toLowerCase();
+    	else whichBoss = 'first';
+    	for(const place of places){
+			if(place.short_name === dungeon){
+				if(place.portTo.hasOwnProperty(whichBoss)){
+					for(const [num, boss] of Object.entries(place.portTo)){
+						if(num === whichBoss){
+							boss.active = !boss.active;
+							if(boss.active) {
+								command.message('[Spawn Change] ' + place.name + ' ' + boss.comment + ' Spawn has been turned ON! It will be turned off in ' + auto_turnoff_time + ' minutes for safety reasons!');
+								clearTimeout(timer);
+								timer = setTimeout(turnoff, auto_turnoff_time*60000);
+							}
+							else command.message('[Spawn Change] ' + place.name + ' ' + boss.comment + ' Spawn has been turned OFF!');
+						}
+						else if (num !== whichBoss && boss.active){
+							boss.active = false;
+							command.message('[Spawn Change] Turned off ' + boss.comment + ' of ' + dungeon + ' to avoid multiple spawn location.')
+						}	
+					}
+					return;
+				}
+				else {
+					command.message('[Spawn Change] ERROR: Unable to find ' + whichBoss + ' in ' + dungeon + '.');
+					return;
+				}
 			}
 		}
-		command.message('[JujuSpawn] ERROR: Unable to find ' + arg + '. Please type help for list of possible dungeons.');
+		command.message('[Spawn Change] ERROR: Unable to find ' + dungeon + '. Please type help for list of possible dungeons.');
+	}
+
+	command.add('SC', (dungeon, whichBoss) => {
+    	processCommand(dungeon, whichBoss);
 	});
 
 	const distance = (A, B) => Math.sqrt(Math.pow(B.x - A.x, 2) + Math.pow(B.y - A.y, 2) + Math.pow(B.z - A.z, 2));
 	const locCheck = (A, B) => A.zone === B.zone && distance(A.loc, B.loc) < 5;
 	const placeCheck = (A) => {
-		for(place of places){
-				if(place.active && locCheck(A,place.entrance)){
+		for(const place of places){
+			for(const boss of Object.values(place.portTo)){
+				if(boss.active && locCheck(A, place.entrance)){	
 					if(!place.allowHealer && isHealer) return;
-					portTo = Object.assign({}, place.portTo);
+					portTo = Object.assign({}, boss);
 					teleport = true;
 					break;
 				}
+			}
 		}
 	};
 
-    dispatch.hook('S_LOGIN', 10, event => {
-        isHealer = (event.templateId % 100 === 7 || event.templateId % 100 === 8) ? true : false;
-    }); 
+	dispatch.game.on('enter_game', () => {
+	 	isHealer = (dispatch.game.me.class === 'priest' || dispatch.game.me.class === 'elementalist') ? true : false;
+
+    });
 
     dispatch.hook('S_LOAD_TOPO', 3, event => {
+    	//console.log(event)
 		if(teleport_enabled) placeCheck(event);
 		if(teleport){
 			event.loc = portTo.loc;
