36d1f4cd73559aac7731efe79c8b46c6f8037649
bobby <sb032835@msn.com>
2018-11-29 01:25:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const places = require('./data.json'),
-      Command = require('command');
+ 'use strict'
+const places = require('./data.json')
 
-module.exports = function SpawnChange(dispatch) {
+
+module.exports = function SpawnChange(mod) {
 	const teleport_enabled = true, 			// Change this to false if you do not want this module on. (Might as well remove it at that point)
 		  auto_turnoff_time = 5;         // Time to Auto-Turn off in minutes.
 
@@ -12,25 +13,24 @@ module.exports = function SpawnChange(dispatch) {
 		teleport,
 		timer;
 
-	const command = Command(dispatch);
 	const turnoff = () => {
-		command.message('[Spawn Change] Turning off following dungeon bosses for safety reasons:');
+		mod.command.message('[Spawn Change] Turning off following dungeon bosses for safety reasons:');
 		for(const place of places){
 			for(const boss of Object.values(place.portTo)){
 				if(place.name != 'Ghiliglade' && boss.active){
 					boss.active = false;
-					command.message(' ' + boss.comment + ' of ' + place.name);
+					mod.command.message(' ' + boss.comment + ' of ' + place.name);
 				}
 			}
 		}
-		command.message('[Spawn Change] Auto-turn off complete.');
+		mod.command.message('[Spawn Change] Auto-turn off complete.');
 	};
 	const processCommand = (dungeon, whichBoss) => {
 		dungeon = dungeon.toUpperCase();
     	if(dungeon === 'HELP'){
     		let str = '[Spawn Change] Possible dungeons are:';
     		for(place of places) str = str + ' ' + place.short_name;
-			command.message(str);
+			mod.command.message(str);
 			return;
     	}
     	if(whichBoss) whichBoss = whichBoss.toLowerCase();
@@ -42,29 +42,29 @@ module.exports = function SpawnChange(dispatch) {
 						if(num === whichBoss){
 							boss.active = !boss.active;
 							if(boss.active) {
-								command.message('[Spawn Change] ' + place.name + ' ' + boss.comment + ' Spawn has been turned ON! It will be turned off in ' + auto_turnoff_time + ' minutes for safety reasons!');
+								mod.command.message('[Spawn Change] ' + place.name + ' ' + boss.comment + ' Spawn has been turned ON! It will be turned off in ' + auto_turnoff_time + ' minutes for safety reasons!');
 								clearTimeout(timer);
 								timer = setTimeout(turnoff, auto_turnoff_time*60000);
 							}
-							else command.message('[Spawn Change] ' + place.name + ' ' + boss.comment + ' Spawn has been turned OFF!');
+							else mod.command.message('[Spawn Change] ' + place.name + ' ' + boss.comment + ' Spawn has been turned OFF!');
 						}
 						else if (num !== whichBoss && boss.active){
 							boss.active = false;
-							command.message('[Spawn Change] Turned off ' + boss.comment + ' of ' + dungeon + ' to avoid multiple spawn location.')
+							mod.command.message('[Spawn Change] Turned off ' + boss.comment + ' of ' + dungeon + ' to avoid multiple spawn location.')
 						}	
 					}
 					return;
 				}
 				else {
-					command.message('[Spawn Change] ERROR: Unable to find ' + whichBoss + ' in ' + dungeon + '.');
+					mod.command.message('[Spawn Change] ERROR: Unable to find ' + whichBoss + ' in ' + dungeon + '.');
 					return;
 				}
 			}
 		}
-		command.message('[Spawn Change] ERROR: Unable to find ' + dungeon + '. Please type help for list of possible dungeons.');
+		mod.command.message('[Spawn Change] ERROR: Unable to find ' + dungeon + '. Please type help for list of possible dungeons.');
 	}
 
-	command.add('go', (dungeon, whichBoss) => {
+	mod.command.add('go', (dungeon, whichBoss) => {
     	processCommand(dungeon, whichBoss);
 	});
 
@@ -83,12 +83,12 @@ module.exports = function SpawnChange(dispatch) {
 		}
 	};
 
-	dispatch.game.on('enter_game', () => {
-	 	isHealer = (dispatch.game.me.class === 'priest' || dispatch.game.me.class === 'elementalist') ? true : false;
+	mod.game.on('enter_game', () => {
+	 	isHealer = (mod.game.me.class === 'priest' || mod.game.me.class === 'elementalist') ? true : false;
 
     });
 
-    dispatch.hook('S_LOAD_TOPO', 3, event => {
+    mod.hook('S_LOAD_TOPO', 3, event => {
     	//console.log(event)
 		if(teleport_enabled) placeCheck(event);
 		if(teleport){
@@ -97,7 +97,7 @@ module.exports = function SpawnChange(dispatch) {
 		}
     });
 
-    dispatch.hook('S_SPAWN_ME', 2, event => {
+    mod.hook('S_SPAWN_ME', 2, event => {
     	//console.log(event)
         if(teleport){
 	    	event.loc = portTo.loc;
